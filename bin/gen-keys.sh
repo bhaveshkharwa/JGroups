1cf9bf0688e57d7b2014e46bdf29bc0bd10c2b87
Bela Ban <belaban@mailbox.org>
2019-03-01 11:58:00 +0100

    ns

--- bin/gen-keys.sh
+++ bin/gen-keys.sh
@@ -5,12 +5,21 @@
 # Author: Tristan Tarrant
 
 
-CA_KS=ca.jks
-ROGUE_CA_KS=rogue-ca.jks
-GOOD_SERVER_KS=good-server.jks
-ROGUE_SERVER_KS=rogue-server.jks
-CA_CERT=ca.cer
-ROGUE_CA_CERT=rogue-ca.cer
+KEYSTORE_DIR=`dirname $0`/../keystore
+CA_KS=$KEYSTORE_DIR/ca.jks
+ROGUE_CA_KS=$KEYSTORE_DIR/rogue-ca.jks
+GOOD_SERVER_KS=$KEYSTORE_DIR/good-server.jks
+ROGUE_SERVER_KS=$KEYSTORE_DIR/rogue-server.jks
+CA_CERT=$KEYSTORE_DIR/ca.cer
+ROGUE_CA_CERT=$KEYSTORE_DIR/rogue-ca.cer
+SERVER_CSR=$KEYSTORE_DIR/server.csr
+SERVER_CER=$KEYSTORE_DIR/server.cer
+ROGUE_CSR=$KEYSTORE_DIR/rogue.csr
+ROGUE_CER=$KEYSTORE_DIR/rogue.cer
+REAL_DIR=`realpath $KEYSTORE_DIR`
+
+
+mkdir -p $KEYSTORE_DIR
 
 # generate CA
 keytool -genkeypair -alias ca -dname cn=ca -validity 10000 -keyalg RSA -keysize 2048 -ext bc:c -keystore $CA_KS -keypass password -storepass password
@@ -27,12 +36,12 @@ keytool -exportcert -keystore $ROGUE_CA_KS -alias rogue_ca -storepass password -
 
 # generate a trusted key for the server and sign it with the CA
 keytool -genkeypair -alias server -dname cn=server -validity 10000 -keyalg RSA -keysize 2048 -keystore $GOOD_SERVER_KS -keypass password -storepass password
-keytool -certreq -alias server -dname cn=server -keystore $GOOD_SERVER_KS -file server.csr -keypass password -storepass password
-keytool -gencert -alias ca -keystore $CA_KS -infile server.csr -outfile server.cer -keypass password -storepass password
+keytool -certreq -alias server -dname cn=server -keystore $GOOD_SERVER_KS -file $SERVER_CSR -keypass password -storepass password
+keytool -gencert -alias ca -keystore $CA_KS -infile $SERVER_CSR -outfile $SERVER_CER -keypass password -storepass password
 
 # import the server cert chain into $GOOD_SERVER_KS (CA and server cert)
 keytool -importcert -alias ca -keystore $GOOD_SERVER_KS -file $CA_CERT -keypass password -storepass password -noprompt
-keytool -importcert -alias server -keystore $GOOD_SERVER_KS -file server.cer -keypass password -storepass password
+keytool -importcert -alias server -keystore $GOOD_SERVER_KS -file $SERVER_CER -keypass password -storepass password
 
 # generate an untrusted key for another server (don't sign it)
 # keytool -genkeypair -alias rogue -dname cn=rogue -validity 10000 -keyalg RSA -keysize 2048 -keystore $ROGUE_SERVER_KS -keypass password -storepass password
@@ -40,9 +49,14 @@ keytool -importcert -alias server -keystore $GOOD_SERVER_KS -file server.cer -ke
 
 # generate a untrusted key for the rogue and sign it with the rogue CA
 keytool -genkeypair -alias rogue -dname cn=rogue -validity 10000 -keyalg RSA -keysize 2048 -keystore $ROGUE_SERVER_KS -keypass password -storepass password
-keytool -certreq -alias rogue -dname cn=rogue -keystore $ROGUE_SERVER_KS -file rogue.csr -keypass password -storepass password
-keytool -gencert -alias rogue_ca -keystore $ROGUE_CA_KS -infile rogue.csr -outfile rogue.cer -keypass password -storepass password
+keytool -certreq -alias rogue -dname cn=rogue -keystore $ROGUE_SERVER_KS -file $ROGUE_CSR -keypass password -storepass password
+keytool -gencert -alias rogue_ca -keystore $ROGUE_CA_KS -infile $ROGUE_CSR -outfile $ROGUE_CER -keypass password -storepass password
 
 # import the rogue cert chain into $ROGUE_SERVER_KS (CA and rogue cert)
 keytool -importcert -alias rogue_ca -keystore $ROGUE_SERVER_KS -file $ROGUE_CA_CERT -keypass password -storepass password -noprompt
-keytool -importcert -alias rogue -keystore $ROGUE_SERVER_KS -file rogue.cer -keypass password -storepass password
+keytool -importcert -alias rogue -keystore $ROGUE_SERVER_KS -file $ROGUE_CER -keypass password -storepass password
+
+
+echo "Generated $GOOD_SERVER_KS and $ROGUE_SERVER_KS in $REAL_DIR"
+echo "Cleaning up"
+rm -f $KEYSTORE_DIR/*.csr $KEYSTORE_DIR/*.cer $KEYSTORE_DIR/*ca.jks
